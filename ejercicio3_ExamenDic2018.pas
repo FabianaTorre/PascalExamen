7c1beb70ad5cb4dc8ae45bc69344392c00a98383
Fabiana Torre <fabiana.torre@estudiante.ceibal.edu.uy>
2019-12-04 21:01:48 -0200

    Solucion examen Pascal Dic 2018 Ej 3

---
+++ ejercicio3_ExamenDic2018.pas
@@ -0,0 +1,46 @@
+﻿Program AddNums(output);
+type lista = ^Nodo;
+nodo = record
+        elemento :Integer;
+        siguiente : lista;
+        end;
+var li: lista;
+num: Integer;
+procedure sumatotal(var l: lista);
+var p: lista;
+suma : Integer;
+
+begin
+suma:= 0;
+if l = Nil then begin
+    new(l);
+    l^.elemento:= suma;
+    l^.siguiente := Nil;
+end
+else
+    p:= l;
+    while (p <> nil) do
+    begin
+        suma := suma + p^.elemento;
+        p:= p^.siguiente;
+    end;
+
+new(p);
+p^.elemento := suma;
+p^.siguiente := nil;
+l:= p;
+end;
+
+begin
+while (num <> 0) do begin
+read(num);
+writeln(num);
+li:= Nil;
+new(li);
+li^.elemento := num;
+li^.siguiente:= li;
+writeln(li^.elemento);
+end;
+
+sumatotal(li);
+end.
